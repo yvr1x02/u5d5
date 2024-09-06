8946ea82477eecfdfab39baf5e225b3031f6addc
Yuri Impellizzeri <128586274+yvr1x02@users.noreply.github.com>
2024-09-06 10:48:06 +0200

    Initial commit

---
+++ src/main/java/yuriimpellizzeri/u5d5/U5d5Application.java
@@ -0,0 +1,13 @@
+package yuriimpellizzeri.u5d5;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class U5d5Application {
+
+	public static void main(String[] args) {
+		SpringApplication.run(U5d5Application.class, args);
+	}
+
+}
---
+++ src/test/java/yuriimpellizzeri/u5d5/U5d5ApplicationTests.java
@@ -0,0 +1,13 @@
+package yuriimpellizzeri.u5d5;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class U5d5ApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
